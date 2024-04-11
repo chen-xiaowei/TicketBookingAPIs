2e6f947d5ad12a5feb90f971999adc51f49b9527
chen-xiaowei <276489887@qq.com>
2024-04-11 11:59:19 +0800

    Add files via upload
    add comments

--- TicketBookingAPIs/Services/AccessToken.cs
+++ TicketBookingAPIs/Services/AccessToken.cs
@@ -4,9 +4,21 @@ namespace TicketBookingAPIs.Services
 {
     public class AccessTokenObject
     {
+        /// <summary>
+        ///  A type of token used for authentication and authorization
+        /// </summary>
         public string AccessToken { get; set; }
+        /// <summary>
+        /// A longer validity period token that is to facilitate the process of obtaining a new AccessToken when the existing one expires.
+        /// </summary>
         public string RefreshToken { get; set; }
+        /// <summary>
+        /// we use the type of Bearer in this case
+        /// </summary>
         public string TokenType { get; set; }
+        /// <summary>
+        /// Expiration date of the token
+        /// </summary>
         public int ExpiresIn { get; set; }
 
         public AccessTokenObject()
@@ -22,7 +34,13 @@ namespace TicketBookingAPIs.Services
     }
     public class RefreshTokenRequest
     {
+        /// <summary>
+        ///  A type of token used for authentication and authorization
+        /// </summary>
         public string AccessToken { get; set; }
+        /// <summary>
+        /// A longer validity period token that is to facilitate the process of obtaining a new AccessToken when the existing one expires.
+        /// </summary>
         public string RefreshToken { get; set; }
     }
 }
